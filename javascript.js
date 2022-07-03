d8b5dcccd8273a4f7297c3f53648f0a732dd8b66
emeteve <uwucatgirlmeow@gmail.com>
2022-07-03 13:18:45 -0400

    Add game function

--- javascript.js
+++ javascript.js
@@ -1,21 +1,28 @@
+let computerChoice = 0;
+let selection = 0;
+let computerScore = 0;
+let playerScore = 0;
+let i = 0;
+let result = 0;
+
 // create function computerPlay to randomly choose options
 function computerPlay() {
-    let computerChoice = 0;
     let computerRandom = Math.floor(Math.random() * 3) + 1;
     if (computerRandom === 1) {
         computerChoice = 'Rock';
+        return computerChoice;
     } else if (computerRandom === 2) {
         computerChoice = 'Paper';
+        return computerChoice;
     } else {
-        computerChoice = 'Scissors'
+        computerChoice = 'Scissors';
+        return computerChoice;
     }
-    
-    return computerChoice;
 }
 
 // prompt player choice in playerSelection
 function playerSelection() {
-    let selection = prompt('Rock Paper Scissors!');
+    selection = prompt('Rock Paper Scissors!');
 
     // make cancel actually cancel
     if (selection === null || selection === '') {
@@ -29,7 +36,6 @@ function playerSelection() {
 
     // returns selection if it is an option
     if (selection === 'Rock' || selection === 'Paper' || selection === 'Scissors' || selection === 'Scissor') {
-        console.log(selection);
         return selection;
     } else {
         // gives another chance to choose
@@ -39,29 +45,78 @@ function playerSelection() {
         
         // prompts for final selection
         if (selection === 'Rock' || selection === 'Paper' || selection === 'Scissors' || selection === 'Scissor') {
-            console.log(selection);
             return selection;
         } else { // return error if they cancel or choose something else
             alert('Error')
+            return null;
         }
+    }
+}
+
+function rockPaperScissors(selection, computerChoice) {
+    selection = playerSelection();
+    computerChoice = computerPlay();
+    if (selection === 'Rock' && computerChoice === 'Scissors') {
+        alert('You Win! Rock beats Scissor!');
+        result = 'You Win! Rock beats Scissor!';
+        playerScore = playerScore + 1;
+    } else if (selection === 'Rock' && computerChoice === 'Paper') {
+        alert('You Lose! Paper beats Rock!');
+        result = 'You Lose! Paper beats Rock!';
+        computerScore = computerScore + 1;
+    } else if (selection === 'Rock' && computerChoice === 'Rock') {
+        alert('Draw!');
+        result = 'Draw!';
+        i = i - 1;
+    } else if (selection === 'Paper' && computerChoice === 'Scissors') {
+        alert('You Lose! Scissor beats Paper!');
+        result = 'You Lose! Scissor beats Paper!';
+        computerScore = computerScore + 1;
+    } else if (selection === 'Paper' && computerChoice === 'Rock') {
+        alert('You Win! Paper beats Rock!');
+        result = 'You Win! Paper beats Rock!';
+        playerScore = playerScore + 1;
+    }  else if (selection === 'Paper' && computerChoice === 'Paper') {
+        alert('Draw!');
+        result = 'Draw!';
+        i = i - 1;
+    }  else if ((selection === 'Scissor' || selection === 'Scissors') && computerChoice === 'Rock') {
+        alert('You Lose! Rock beats Scissor!');
+        result = 'You Lose! Rock beats Scissor!';
+        computerScore = computerScore + 1;
+    }  else if ((selection === 'Scissor' || selection === 'Scissors') && computerChoice === 'Paper') {
+        alert('You Win! Scissor beats Paper!');
+        result = 'You Win! Scissor beats Paper!';
+        playerScore = playerScore + 1;
+    }  else if ((selection === 'Scissor' || selection === 'Scissors') && computerChoice === 'Scissors') {
+        alert('Draw!');
+        result = 'Draw!';
+        i = i - 1;
     }
 }
 
 // create rockPaperScissors that decides who wins round
 // repeat the functions 5 times and logs score
 function game() {
-    for (let i = 0; i < 5; i++) {
-        function rockPaperScissors(playerSelection, computerPlay) {
-            if (selection === null || selection === '') {
-                selection = null;
-                alert('Canceled');
-                return selection;
-            }
+    for (i = 0; i < 5; i++) {
+        rockPaperScissors();
+        if (selection === null || selection === '') {
+            i = i + 100;
+        } 
+        console.log(result);
+        console.log(`Player Score: ${playerScore}`);
+        console.log(`Computer Score: ${computerScore}`);
+        if (computerScore === 3) {
+            alert('You Lost Rock Paper Scissors!');
+            console.log('You Lost Rock Paper Scissors!');
+            i = i + 100;
+        } else if (playerScore === 3) {
+            alert('You Won Rock Paper Scissors!');
+            console.log('You Won Rock Paper Scissors!');
+            i = i + 100;
         }
     }
 }
 
-computerPlay();
-playerSelection();
 game();
 
